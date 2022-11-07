8ff34154f7a0476c6a7640204cefca975a640cf4
Daniel Chalmers <daniel.chalmers@outlook.com>
2022-11-06 21:46:20 -0600

    Clearer theme names for Light and Dark

--- DesktopClock/App.xaml.cs
+++ DesktopClock/App.xaml.cs
@@ -11,8 +11,8 @@ public partial class App : Application
     // https://www.materialui.co/colors
     public static IReadOnlyList<Theme> Themes { get; } = new[]
     {
-        new Theme("Light", "#F5F5F5", "#212121"),
-        new Theme("Dark", "#212121", "#F5F5F5"),
+        new Theme("Light Text", "#F5F5F5", "#212121"),
+        new Theme("Dark Text", "#212121", "#F5F5F5"),
         new Theme("Red", "#D50000", "#FF8A80"),
         new Theme("Pink", "#C51162", "#FF80AB"),
         new Theme("Purple", "#AA00FF", "#EA80FC"),
