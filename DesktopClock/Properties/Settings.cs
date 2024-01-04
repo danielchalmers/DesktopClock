27ef7002e32638ffc3e6913fedc7ad54a638b692
Daniel Chalmers <daniel.chalmers@outlook.com>
2024-01-03 19:49:48 -0600

    Add background image advanced option
    Closes #36

--- DesktopClock/Properties/Settings.cs
+++ DesktopClock/Properties/Settings.cs
@@ -69,6 +69,7 @@ public sealed class Settings : INotifyPropertyChanged, IDisposable
     public bool BackgroundEnabled { get; set; } = true;
     public double BackgroundOpacity { get; set; } = 0.90;
     public double BackgroundCornerRadius { get; set; } = 1;
+    public string BackgroundImagePath { get; set; } = string.Empty;
     public double OutlineThickness { get; set; } = 0.2;
     public bool Topmost { get; set; } = true;
     public bool ShowInTaskbar { get; set; } = true;
