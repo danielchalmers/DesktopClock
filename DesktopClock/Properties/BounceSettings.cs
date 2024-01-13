2578e335b3c1aadd42eb234352e080ace34f6229
Nico Baumgarten <Nico@homebusiness.dyndns.org>
2024-01-13 14:11:08 +0100

    + Added simple Bounce option to prevent burn-in in displays

---
+++ DesktopClock/Properties/BounceSettings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopClock.Properties;
+public sealed class BounceSettings : INotifyPropertyChanged
+{
+
+#pragma warning disable CS0067 // The event 'Settings.PropertyChanged' is never used
+    public event PropertyChangedEventHandler PropertyChanged;
+#pragma warning restore CS0067 // The event 'Settings.PropertyChanged' is never used
+
+
+    public bool Enabled { get; set; } = false;
+
+    public double HorizontalBounce { get; set; } = 100.0;
+    public double VerticalBounce { get; set; } = 50.0;
+
+
+    public TimeSpan Interval { get; set; } = TimeSpan.FromMinutes(2);
+
+
+}
