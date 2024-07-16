f6b8c770203c9f3fe91bb1749a3167affbb17a04
Daniel Chalmers <daniel.chalmers@outlook.com>
2024-07-15 22:46:15 -0500

    wip

---
+++ DesktopClock/SettingsWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using Newtonsoft.Json;
+using DesktopClock.Properties;
+
+namespace DesktopClock
+{
+    public partial class SettingsWindow : Window
+    {
+        public SettingsWindow()
+        {
+            InitializeComponent();
+            DataContext = Settings.Default;
+        }
+    }
+
+    public class SettingsViewModel : INotifyPropertyChanged
+    {
+        public Settings Settings { get; }
+
+        public SettingsViewModel(Settings settings)
+        {
+            Settings = settings;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public RelayCommand SaveCommand => new RelayCommand(SaveSettings);
+
+        private void SaveSettings(object parameter)
+        {
+            Settings.Save();
+            MessageBox.Show("Settings saved.");
+        }
+    }
+
+    public class RelayCommand : System.Windows.Input.ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) => _canExecute?.Invoke(parameter) ?? true;
+
+        public void Execute(object parameter) => _execute(parameter);
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+}
