55c4c41c604ccb4b7781a7026ada572c34eb1900
Daniel Chalmers <daniel.chalmers@outlook.com>
2026-05-02 10:16:22 -0500

    catch all

--- DesktopClock/MainWindow.xaml.cs
+++ DesktopClock/MainWindow.xaml.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Media;
-using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -73,7 +72,7 @@ public partial class MainWindow : Window
         {
             Clipboard.SetText(CurrentTimeOrCountdownString);
         }
-        catch (ExternalException)
+        catch
         {
             _trayIcon?.ShowNotification("Copy failed", "ouldn't update the clipboard. Try again.");
         }
